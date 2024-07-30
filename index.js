a0beaa0d50a84360f65bee114e2b4a9800e1111f
jackboys910 <george.jackboys@gmail.com>
2024-07-30 23:36:10 +0300

    feat: added PostgreSQL, security and new commands

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
+const { Client } = require('pg')
 const TelegramBot = require('node-telegram-bot-api')
 const { gameOptions, againOptions } = require('./options')
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+})
+client.connect()
+
 const token = process.env.TELEGRAM_BOT_TOKEN
 
 const bot = new TelegramBot(token, { polling: true })
 
 const chats = {}
+
+let startExecuted = false
+let topicInStack = false
 
 const startGame = async (chatId) => {
   await bot.sendMessage(
@@ -19,21 +29,31 @@ const startGame = async (chatId) => {
 
 const start = () => {
   bot.setMyCommands([
-    { command: '/start', description: 'Начальное приветствие' },
     { command: '/info', description: 'Получить информацию о пользователе' },
     { command: '/game', description: 'Игра угадай цифру' },
+    { command: '/addtopic', description: 'Добавить тему для изучения' },
+    { command: 'seetopics', description: 'Посмотреть все темы' },
   ])
 
   bot.on('message', async (msg) => {
     const text = msg.text
     const chatId = msg.chat.id
 
-    if (text === '/start') {
+    if (text === '/start' && !startExecuted) {
+      startExecuted = true
       await bot.sendSticker(
         chatId,
         'https://sl.combot.org/cryptonotes/webp/0xf09f9690.webp'
       )
-      return bot.sendMessage(chatId, `Привет.`)
+      return bot.sendMessage(
+        chatId,
+        'Можешь посмотреть список доступных команд в меню ниже'
+      )
+    } else if (text === '/start' && startExecuted) {
+      return bot.sendMessage(
+        chatId,
+        'Мы уже здоровались, доступные команды написаны в меню ниже'
+      )
     }
 
     if (text === '/info') {
@@ -42,6 +62,28 @@ const start = () => {
 
     if (text === '/game') {
       return startGame(chatId)
+    }
+
+    if (text === '/addtopic') {
+      topicInStack = true
+      return bot.sendMessage(chatId, 'Напишите тему для добавления в список')
+    }
+
+    if (text === '/seetopics') {
+      const res = await client.query('SELECT * FROM topics ORDER BY created_at')
+      const topics = res.rows
+        .map(
+          (row, index) =>
+            `${index + 1}. ${row.topic} (добавлено: ${row.created_at})`
+        )
+        .join('\n')
+      return bot.sendMessage(chatId, `Темы:\n${topics}`)
+    }
+
+    if (topicInStack) {
+      topicInStack = false
+      await client.query('INSERT INTO topics (topic) VALUES ($1)', [text])
+      return bot.sendMessage(chatId, 'Тема успешно добавлена в список!')
     }
 
     return bot.sendMessage(chatId, 'Я тебя не понимаю, напиши команду')
